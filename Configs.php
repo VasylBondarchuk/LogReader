7cc63327195c20aec157f55a4dd663b89cbb44b8
VasylBondarchuk <teorfiz@gmail.com>
2023-02-13 23:14:33 +0200

    Add congigs

--- Configs.php
+++ Configs.php
@@ -6,4 +6,6 @@ class Configs
 {
     const LOG_DIR_PATH = BP. DIRECTORY_SEPARATOR . 'var' . DIRECTORY_SEPARATOR .'log';
     const DEFAULT_LINES_QTY = 10;
+    const FILE_NAME_REQUEST_FIELD = 'file_name';
+    const LINES_QTY_REQUEST_FIELD = 'lines_qty';
 }
